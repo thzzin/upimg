bc62b0260cfb81cf402b262591846464f82db3af
thzzin <thithipaiva@hotmail.com>
2024-10-02 04:33:58 -0300

    variaveis

--- src/audio/audio.module.ts
+++ src/audio/audio.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AudioService } from './audio.service';
 import { AudioController } from './audio.controller';
+import {UploadController} from '../upload/upload.controller'
 @Module({
-  controllers: [AudioController],
+  controllers: [AudioController, UploadController],
   providers: [AudioService],
 })
 export class ImagesModule {}
